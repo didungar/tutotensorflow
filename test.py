9fd7a59e6a4bebfd2355ce20d46817a16868146b
Didier Ungar <didier.ungar@gmail.com>
2018-08-18 14:21:17 +0200

    import de donnée externe

--- test.py
+++ test.py
@@ -1,69 +1,88 @@
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
+import csv
+
+def get_features_column_count():
+	return 3;
+def get_targets_column_count():
+	return 1;
 
 def get_dataset():
-    """
-        Method used to generate the dataset
-    """
-    # Numbers of row per class
-    row_per_class = 100
-    # Generate rows
-    sick = np.random.randn(row_per_class, 2) + np.array([-2, -2])
-    sick_2 = np.random.randn(row_per_class, 2) + np.array([2, 2])
+        """
+                Method used to generate the dataset
+        """
+	features = []
+	targets = []
 
-    healthy = np.random.randn(row_per_class, 2) + np.array([-2, 2])
-    healthy_2 = np.random.randn(row_per_class, 2) + np.array([2, -2])
+        f = open("data.csv", "r")
+        reader = csv.reader(f, delimiter=',')
+        for row in reader:
+                features.append(row)
+	print "features\n", features, "\n"
 
-    features = np.vstack([sick, sick_2, healthy, healthy_2])
-    targets = np.concatenate((np.zeros(row_per_class * 2), np.zeros(row_per_class * 2) + 1))
+        f = open("result.csv", "r")
+        reader = csv.reader(f, delimiter=',')
+        for row in reader:
+                targets.append(row)
+	print "targets\n", targets, "\n"
 
-    targets = targets.reshape(-1, 1)
-
-    return features, targets
+        return features, targets
 
 if __name__ == '__main__':
-    features, targets = get_dataset()
-    # Plot points
-    #plt.scatter(features[:, 0], features[:, 1], s=40, c=targets, cmap=plt.cm.Spectral)
-    #plt.show()
+        features, targets = get_dataset()
+        sess = tf.Session()
 
-    tf_features = tf.placeholder(tf.float32, shape=[None, 2])
-    tf_targets = tf.placeholder(tf.float32, shape=[None, 1])
+        # Plot points
+        #plt.scatter(features[:, 0], features[:, 1], s=40, c=targets, cmap=plt.cm.Spectral)
+        #plt.show()
+	
+	# tf_features = tf.placeholder(tf.float32, shape=[lentgh, Nb of data for row])
+        tf_features = tf.placeholder(tf.float32, shape=[None, get_features_column_count()])
+	# tf_targets = tf.placeholder(tf.float32, shape=[lentgh, Nb of result for row])
+        tf_targets = tf.placeholder(tf.float32, shape=[None, get_targets_column_count()])
 
-    # First
-    w1 = tf.Variable(tf.random_normal([2, 3]))
-    b1 = tf.Variable(tf.zeros([3]))
-    # Operations
-    z1 = tf.matmul(tf_features, w1) + b1
-    a1 = tf.nn.sigmoid(z1)
+	# features Line
+	flp = tf.Variable(tf.random_normal([get_features_column_count(), 1]))
+	fls = tf.Variable(tf.random_normal([get_features_column_count(), 1]))
+	# features prediction
+	fp = tf.matmul(tf_features, flp) + fls
+	# error prediction
+	ep  = tf.reduce_mean(tf.square(fp - tf_targets))
+	# optimiseur
+	train_op = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(ep)
+	# accuracy (ratio juste)
+	correct_prediction = tf.equal(tf.round(fp), tf_targets)
+	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-    # Output neuron
-    w2 = tf.Variable(tf.random_normal([3, 1]))
-    b2 = tf.Variable(tf.zeros([1]))
-    # Operations
-    z2 = tf.matmul(a1, w2) + b2
-    py = tf.nn.sigmoid(z2)
+	# Learning :
+        sess.run(tf.global_variables_initializer())
+	
 
-    cost = tf.reduce_mean(tf.square(py - tf_targets))
-
-    correct_prediction = tf.equal(tf.round(py), tf_targets)
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
-    train = optimizer.minimize(cost)
-
-    sess = tf.Session()
-    sess.run(tf.global_variables_initializer())
-
-    for e in range(10000):
-
-        sess.run(train, feed_dict={
-            tf_features: features,
-            tf_targets: targets
+        sess.run(tf_features, feed_dict={
+                tf_features: [features[0]],
+                tf_targets: [targets[0]]
         })
+        print "flp = ", sess.run(flp), "\n"
+        print "fls = ", sess.run(fls), "\n"
+        print "fp = ", sess.run(fp, feed_dict={
+                tf_features: [features[0]],
+                tf_targets: [targets[0]]
+        }), "\n"
+        print "ep = ", sess.run(ep, feed_dict={
+                tf_features: [features[0]],
+                tf_targets: [targets[0]]
+        }), "\n"
+        print "train_op = ", sess.run(train_op, feed_dict={
+                tf_features: [features[0]],
+                tf_targets: [targets[0]]
+        }), "\n"
+        print "correct_prediction = ", sess.run(correct_prediction, feed_dict={
+                tf_features: [features[0]],
+                tf_targets: [targets[0]]
+        }), "\n"
+        print "accuracy = ", sess.run(accuracy, feed_dict={
+                tf_features: [features[0]],
+                tf_targets: [targets[0]]
+        }), "\n"
 
-        print("accuracy =", sess.run(accuracy, feed_dict={
-            tf_features: features,
-            tf_targets: targets
-        }))
